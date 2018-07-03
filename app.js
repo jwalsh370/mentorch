e69e1cc9cb5b8899e6c6af87c0c433518ec1c61b
jahan walsh <jahan.walsh@maxxpotential.com>
2018-07-03 15:27:31 -0400

    add zoom view page.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var Peer = require('simple-peer');
 var firebase = require("firebase");
 var admin = require("firebase-admin");
 var serviceAccount = require('./mentorch-84acd-firebase-adminsdk-rv8ny-9520ca82cb.json');
+var zoom_key = 'ooHs4258TzCbPuGkmb2Z3Q';
+var zoom_sec = 'i3X2uAURQH9UK5vXk36cxVm7KtGUOUZydYoL';
+var thenrequest = require('then-request');
+var async = require('async');
+var jwt = require('jsonwebtoken');
+
+
+
 
 
 admin.initializeApp({
@@ -165,8 +173,124 @@ app.get('/settings', function(req, res){
   res.render('settings', {
   });
 })
+// ZOOM CODE START
 
+app.get('/zoomView', function(req, res) {
+  res.render('zoom', {title: 'Connect with Zoom'});
+});
 
+app.get('/createUser', function(req, res) {
+  res.render('users', {title: 'User Management'});
+});
+
+app.post('/createUser', function(req, res) {
+
+  var options = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", email: req.body.email , type: 2}
+  };
+
+  // make an asynchronous request to zoom to create a User
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/user/create",options).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+app.get('/autoUser', function(req, res) {
+  res.render('autoUsers', {title: 'User Management'});
+});
+
+app.post('/autoUser', function(req, res) {
+  console.log(req.body);
+  console.log("email:", req.body.email);
+  var options = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", email: req.body.email , password: req.body.pwd, type: 2}
+  };
+
+  // make an asynchronous request to zoom to create a user without email verification
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/user/autocreate2",options).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+app.get('/updateUser', function(req, res) {
+  res.render('upUsers', {title: 'User Management'});
+});
+
+app.post('/updateUser', function(req, res) {
+  console.log(req.body);
+  console.log("email:", req.body.id);
+
+  var options = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", id: req.body.id , type: req.body.type}
+  };
+
+  // make an asynchronous request to zoom to update a user
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/user/update",options).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+app.get('/createMeeting', function(req, res) {
+  res.render('Meetings', {title: 'Manage Meetings'});
+});
+
+app.post('/createMeeting', function(req, res) {
+  console.log(req.body);
+  console.log("id:", req.body.id);
+
+  console.log("topic:", req.body.topic);
+   var Moptions = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id , topic: req.body.topic, type: 3}
+  };
+
+  // make an asynchronous request to zoom to create a meeting
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/meeting/create",Moptions).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+app.get('/listMeeting', function(req, res) {
+  res.render('listMeetings', {title: 'Manage Meetings'});
+});
+
+app.post('/listMeeting', function(req, res) {
+  console.log(req.body);
+  console.log("id:", req.body.id);
+
+  var Moptions = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id }
+  };
+  // make an asynchronous request to zoom to list all meetings
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/meeting/list",Moptions).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+app.get('/updateMeeting', function(req, res) {
+  res.render('upMeetings', {title: 'Manage Meetings'});
+});
+
+app.post('/updateMeeting', function(req, res) {
+  console.log(req.body);
+  console.log("id:", req.body.id);
+
+  console.log("topic:", req.body.topic);
+  var Moptions = {
+    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id , id: req.body.mId, type: req.body.type}
+  };
+  // make an asynchronous request to zoom to update a meeting
+  var asyncres = thenrequest('POST',"https://dev.zoom.us/v1/meeting/update",Moptions).done(function (res) {
+    console.log(res.getBody('utf8'));
+    });
+  res.redirect('/');
+});
+
+// ZOOM END
 
 app.all('/call', function(req, res){
 
